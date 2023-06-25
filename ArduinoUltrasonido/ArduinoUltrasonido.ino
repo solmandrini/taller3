59e60f1d740f1dd6e386e7e879bf7488221c64bc
Belen <66374537+belenignoffo@users.noreply.github.com>
2023-06-25 11:29:24 -0300

    Add files via upload

---
+++ ArduinoUltrasonido/ArduinoUltrasonido.ino
@@ -0,0 +1,29 @@
+/* Mide la distancia del sonido con el sensor ultrasónico */
+
+int trigPin = 12; 
+int echoPin = 11;
+int ledPin = 7;
+int duracion;
+int distancia;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds( 1 );
+  digitalWrite(trigPin, LOW);
+  duracion = pulseIn( echoPin, HIGH ); // devuelve un valor de tiempo en microsegundos
+  distancia = duracion/58.2; // esto devuelve la distancia en centímetros
+  Serial.println( distancia );
+  delay( 200 );
+  if ( distancia <= 160 && distancia >= 0 ){
+    digitalWrite( ledPin, HIGH );
+    delay( distancia );
+    digitalWrite( ledPin, LOW );
+  }
+}
